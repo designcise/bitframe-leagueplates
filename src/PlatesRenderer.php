1664cc21a4ca41957ab86e2c6c98233258869ba9
Designcise <designcise@gmail.com>
2018-05-16 14:23:01 +0500

    Add files via upload

--- src/PlatesRenderer.php
+++ src/PlatesRenderer.php
@@ -28,55 +28,55 @@ use BitFrame\Exception\FileNotReadableException;
  */
 class PlatesRenderer implements TemplateInterface 
 {
-	/** @var Engine */
-	private $template;
-	
-	/**
-	 * @param string|null $templateDir
-	 * @param string $templateExt (optional)
-	 * @param array $data (optional)
-	 *
-	 * @see Engine::__construct()
-	 */
-	public function __construct(?string $templateDir, string $templateExt = 'tpl', array $data = []) 
-	{
-		$this->template = new Engine($templateDir, $templateExt);
-		
-		if (! empty($data)) {
-			$this->addDefaultParam(self::TEMPLATE_ALL, $data);
-		}
-		
-		return $this;
-	}
-	
-	/**
-	 * Render template.
-	 *
-	 * @param string $templateName
-	 * @param array $data (optional)
-	 *
-	 * @return string
-	 *
-	 * @throws \BitFrame\Exception\FileNotReadableException
-	 */
-	public function render(string $templateName, array $data = []): string 
-	{
-		if ($templateName === '' || ! $this->template->exists($templateName)) {
-			throw new FileNotReadableException($templateName);
-		}
-		
-		// render main tpl
-		$render = $this->template->render($templateName, $data);
-		
-		return $render;
+    /** @var Engine */
+    private $template;
+    
+    /**
+     * @param string|null $templateDir
+     * @param string $templateExt (optional)
+     * @param array $data (optional)
+     *
+     * @see Engine::__construct()
+     */
+    public function __construct(?string $templateDir, string $templateExt = 'tpl', array $data = []) 
+    {
+        $this->template = new Engine($templateDir, $templateExt);
+        
+        if (! empty($data)) {
+            $this->addDefaultParam(self::TEMPLATE_ALL, $data);
+        }
+        
+        return $this;
     }
-	
-	/**
+    
+    /**
+     * Render template.
+     *
+     * @param string $templateName
+     * @param array $data (optional)
+     *
+     * @return string
+     *
+     * @throws \BitFrame\Exception\FileNotReadableException
+     */
+    public function render(string $templateName, array $data = []): string 
+    {
+        if ($templateName === '' || ! $this->template->exists($templateName)) {
+            throw new FileNotReadableException($templateName);
+        }
+        
+        // render main tpl
+        $render = $this->template->render($templateName, $data);
+        
+        return $render;
+    }
+    
+    /**
      * Proxies to the Plate Engine's `addData()` method.
      *
      * {@inheritDoc}
-	 *
-	 * @throws \InvalidArgumentException
+     *
+     * @throws \InvalidArgumentException
      */
     public function addDefaultParam(string $templateName, $params)
     {
@@ -101,15 +101,15 @@ class PlatesRenderer implements TemplateInterface
 
         $this->template->addData($params, $templateName);
     }
-	
-	/**
+    
+    /**
      * {@inheritDoc}
-	 *
+     *
      * @return void
      */
     public function addPath(string $path, ?string $namespace = null): void
     {
-		if (! $namespace && ! $this->template->getDirectory()) {
+        if (! $namespace && ! $this->template->getDirectory()) {
             $this->template->setDirectory($path);
             return;
         }
@@ -121,31 +121,31 @@ class PlatesRenderer implements TemplateInterface
 
         $this->template->addFolder($namespace, $path, true);
     }
-	
-	/**
+    
+    /**
      * {@inheritDoc}
      */
     public function getPaths(): array
     {
-		$paths = ($this->template->getDirectory()) ? [$this->getDefaultPath()] : [];
+        $paths = ($this->template->getDirectory()) ? [$this->getDefaultPath()] : [];
 
         foreach ($this->getPlatesFolders() as $folder) {
             $paths[] = new TemplatePath($folder->getPath(), $folder->getName());
         }
         return $paths;
     }
-	
-	/**
-	 * Get Template Engine class.
-	 *
-	 * @return Engine
-	 */
-	public function getEngine(): Engine
-	{
-		return $this->template;
-	}
-	
-	/**
+    
+    /**
+     * Get Template Engine class.
+     *
+     * @return Engine
+     */
+    public function getEngine(): Engine
+    {
+        return $this->template;
+    }
+    
+    /**
      * Create and return a TemplatePath representing the default Plates directory.
      *
      * @return TemplatePath
@@ -165,7 +165,7 @@ class PlatesRenderer implements TemplateInterface
         $folders = $this->template->getFolders();
         $r = new \ReflectionProperty($folders, 'folders');
         $r->setAccessible(true);
-		
+        
         return $r->getValue($folders);
     }
 }
--- test/PlatesRendererTest.php
+++ test/PlatesRendererTest.php
@@ -25,128 +25,128 @@ use \BitFrame\Renderer\{PlatesRenderer, TemplatePath};
  */
 class PlatesRendererTest extends TestCase
 {
-	/** @var \BitFrame\Renderer\PlatesRenderer */
-	private $tpl;
-	
-	private static $assetDir = __DIR__ . '/Asset';
-	
+    /** @var \BitFrame\Renderer\PlatesRenderer */
+    private $tpl;
+    
+    private static $assetDir = __DIR__ . '/Asset';
+    
     public function setUp()
     {
-		$this->tpl = new PlatesRenderer(self::$assetDir, 'tpl');
+        $this->tpl = new PlatesRenderer(self::$assetDir, 'tpl');
     }
-	
-	public function testCanProvideEngineAtInstantiation()
-	{
-		$renderer = new PlatesRenderer(null);
-		$this->assertInstanceOf(PlatesRenderer::class, $renderer);
-		$this->assertEmpty($renderer->getPaths());
-	}
-	
-	public function testCanAddPath()
-	{
-		$renderer = new PlatesRenderer(null);
-		$renderer->addPath(self::$assetDir);
-		$paths = $renderer->getPaths();
-		
-		$this->assertInternalType('array', $paths);
-		$this->assertCount(1, $paths);
-		$this->assertTemplatePath(self::$assetDir, $paths[0]);
-		$this->assertTemplatePathString(self::$assetDir, $paths[0]);
-		$this->assertEmptyTemplatePathNamespace($paths[0]);
-		
-		return $renderer;
-	}
-	
-	public function testNullDirectory()
-	{
-		$renderer = new PlatesRenderer(null, 'tpl');
-		$paths = $renderer->getPaths();
-		
-		$this->assertEquals(null, $renderer->getEngine()->getDirectory());
-        $this->assertCount(0, $paths);
-	}
-	
-	public function testSetDefaultDirectory()
-	{
-		$dir = __DIR__;
-		$renderer = new PlatesRenderer($dir);
-		$paths = $renderer->getPaths();
-		
-		$this->assertEquals($dir, $renderer->getEngine()->getDirectory());
+    
+    public function testCanProvideEngineAtInstantiation()
+    {
+        $renderer = new PlatesRenderer(null);
+        $this->assertInstanceOf(PlatesRenderer::class, $renderer);
+        $this->assertEmpty($renderer->getPaths());
+    }
+    
+    public function testCanAddPath()
+    {
+        $renderer = new PlatesRenderer(null);
+        $renderer->addPath(self::$assetDir);
+        $paths = $renderer->getPaths();
+        
+        $this->assertInternalType('array', $paths);
         $this->assertCount(1, $paths);
-	}
-	
-	/**
+        $this->assertTemplatePath(self::$assetDir, $paths[0]);
+        $this->assertTemplatePathString(self::$assetDir, $paths[0]);
+        $this->assertEmptyTemplatePathNamespace($paths[0]);
+        
+        return $renderer;
+    }
+    
+    public function testNullDirectory()
+    {
+        $renderer = new PlatesRenderer(null, 'tpl');
+        $paths = $renderer->getPaths();
+        
+        $this->assertEquals(null, $renderer->getEngine()->getDirectory());
+        $this->assertCount(0, $paths);
+    }
+    
+    public function testSetDefaultDirectory()
+    {
+        $dir = __DIR__;
+        $renderer = new PlatesRenderer($dir);
+        $paths = $renderer->getPaths();
+        
+        $this->assertEquals($dir, $renderer->getEngine()->getDirectory());
+        $this->assertCount(1, $paths);
+    }
+    
+    /**
      * @param PlatesRenderer $renderer
      * @depends testCanAddPath
      */
     public function testAddingSecondPathWithoutNamespaceIsANoopAndRaisesWarning($renderer)
     {
-		$paths = $renderer->getPaths();
-		$path  = array_shift($paths);
-		
-		$error = false;
+        $paths = $renderer->getPaths();
+        $path  = array_shift($paths);
+        
+        $error = false;
 
-		set_error_handler(function ($e, $message) use (&$error) {
-			$error = true;
-			$this->assertContains('duplicate', $message);
-			return true;
-		}, E_USER_WARNING);
+        set_error_handler(function ($e, $message) use (&$error) {
+            $error = true;
+            $this->assertContains('duplicate', $message);
+            return true;
+        }, E_USER_WARNING);
 
-		$renderer->addPath(__DIR__);
-		restore_error_handler();
+        $renderer->addPath(__DIR__);
+        restore_error_handler();
 
-		$this->assertTrue($error, 'Error handler was not triggered when calling addPath() multiple times');
+        $this->assertTrue($error, 'Error handler was not triggered when calling addPath() multiple times');
 
-		$paths = $renderer->getPaths();
-		$this->assertInternalType('array', $paths);
-		$this->assertCount(1, $paths);
+        $paths = $renderer->getPaths();
+        $this->assertInternalType('array', $paths);
+        $this->assertCount(1, $paths);
 
-		$test = array_shift($paths);
-		$this->assertEqualTemplatePath($path, $test);
+        $test = array_shift($paths);
+        $this->assertEqualTemplatePath($path, $test);
     }
-	
-	public function testCanAddPathWithNamespace()
+    
+    public function testCanAddPathWithNamespace()
     {
         $renderer = new PlatesRenderer(null);
         $renderer->addPath(self::$assetDir, 'test');
         $paths = $renderer->getPaths();
-		
+        
         $this->assertInternalType('array', $paths);
         $this->assertCount(1, $paths);
         $this->assertTemplatePath(self::$assetDir, $paths[0]);
         $this->assertTemplatePathString(self::$assetDir, $paths[0]);
         $this->assertTemplatePathNamespace('test', $paths[0]);
     }
-	
-	public function assertTemplatePath($path, TemplatePath $templatePath, $message = null)
+    
+    public function assertTemplatePath($path, TemplatePath $templatePath, $message = null)
     {
         $message = $message ?: sprintf('Failed to assert TemplatePath contained path %s', $path);
         $this->assertEquals($path, $templatePath->getPath(), $message);
     }
-	
-	public function testDelegatesRenderingToUnderlyingImplementation()
+    
+    public function testDelegatesRenderingToUnderlyingImplementation()
     {
         $renderer = $this->tpl;
-		
-		$name = 'BitFrame';
+        
+        $name = 'BitFrame';
         $result = $renderer->render('plates', ['name' => $name]);
         $this->assertContains($name, $result);
-		
+        
         $content = file_get_contents(self::$assetDir . '/plates.tpl');
         $content = str_replace('<?= $name; ?>', $name, $content);
         $this->assertEquals($content, $result);
     }
-	
-	public function testCanRenderWithNullParams()
+    
+    public function testCanRenderWithNullParams()
     {
         $renderer = $this->tpl;
         $result = $renderer->render('plates_null');
         $content = file_get_contents(self::$assetDir . '/plates_null.tpl');
         $this->assertEquals($content, $result);
     }
-	
-	/**
+    
+    /**
      * @group namespacing
      */
     public function testProperlyResolvesNamespacedTemplate()
@@ -154,147 +154,147 @@ class PlatesRendererTest extends TestCase
         $renderer = new PlatesRenderer(self::$assetDir, 'tpl');
         $renderer->addPath(self::$assetDir . '/test', 'test');
         $test = $renderer->render('test::namespaced');
-		
+        
         $expected = file_get_contents(self::$assetDir . '/test/namespaced.tpl');
-		
+        
         $this->assertSame($expected, $test);
     }
-	
-	public function testAddParameterToOneTemplate()
+    
+    public function testAddParameterToOneTemplate()
     {
         $renderer = new PlatesRenderer(self::$assetDir, 'tpl');
-		
+        
         $name = 'Plates';
         $renderer->addDefaultParam('plates', ['name' => $name]);
-		
+        
         $result = $renderer->render('plates');
         $content = file_get_contents(self::$assetDir . '/plates.tpl');
         $content = str_replace('<?= $name; ?>', $name, $content);
-		
+        
         $this->assertEquals($content, $result);
-		
-		// @fixme hack to work around https://github.com/thephpleague/plates/issues/60, remove if ever merged
+        
+        // @fixme hack to work around https://github.com/thephpleague/plates/issues/60, remove if ever merged
         set_error_handler(function ($error, $message) {
             $this->assertContains('Undefined variable: name', $message);
             return true;
         }, E_NOTICE);
-		
+        
         $result = $renderer->render('plates2');
-		restore_error_handler();
-		$content = file_get_contents(self::$assetDir . '/plates2.tpl');
+        restore_error_handler();
+        $content = file_get_contents(self::$assetDir . '/plates2.tpl');
         $content = str_replace('<?= $name; ?>', '', $content);
-		
+        
         $this->assertEquals($content, $result);
     }
-	
-	public function testAddSharedParameters()
+    
+    public function testAddSharedParameters()
     {
         $renderer = new PlatesRenderer(self::$assetDir, 'tpl');
-		
+        
         $name = 'Plates';
         $renderer->addDefaultParam($renderer::TEMPLATE_ALL, ['name' => $name]);
-		
+        
         $result = $renderer->render('plates');
         $content = file_get_contents(self::$assetDir . '/plates.tpl');
         $content = str_replace('<?= $name; ?>', $name, $content);
         $this->assertEquals($content, $result);
-		
+        
         $result = $renderer->render('plates2');
         $content = file_get_contents(self::$assetDir . '/plates2.tpl');
         $content = str_replace('<?= $name; ?>', $name, $content);
-		
+        
         $this->assertEquals($content, $result);
     }
-	
-	public function testOverwriteSharedGlobalParameters()
+    
+    public function testOverwriteSharedGlobalParameters()
     {
-		// define a globally available 'name' param to all templates
-		$name = 'Plates';
-		$data = ['name' => 'Plates'];
+        // define a globally available 'name' param to all templates
+        $name = 'Plates';
+        $data = ['name' => 'Plates'];
         $renderer = new PlatesRenderer(self::$assetDir, 'tpl', $data);
-		
+        
         $result = $renderer->render('plates');
         $content = file_get_contents(self::$assetDir . '/plates.tpl');
         $content = str_replace('<?= $name; ?>', $name, $content);
-		
+        
         $this->assertEquals($content, $result);
-		
-		// overwrite name using addDefaultParam
+        
+        // overwrite name using addDefaultParam
         $name = 'BitFrame';
         $renderer->addDefaultParam($renderer::TEMPLATE_ALL, ['name' => $name]);
-		
+        
         $result = $renderer->render('plates');
         $content = file_get_contents(self::$assetDir . '/plates.tpl');
         $content = str_replace('<?= $name; ?>', $name, $content);
-		
+        
         $this->assertEquals($content, $result);
-		
-		// overwrite name using render
-		$name = 'BitFrame Test2';
-		$data = ['name' => $name];
+        
+        // overwrite name using render
+        $name = 'BitFrame Test2';
+        $data = ['name' => $name];
         $result = $renderer->render('plates', $data);
         $content = file_get_contents(self::$assetDir . '/plates.tpl');
         $content = str_replace('<?= $name; ?>', $name, $content);
-		
+        
         $this->assertEquals($content, $result);
     }
-	
-	public function testOverwriteSharedParametersPerTemplate()
+    
+    public function testOverwriteSharedParametersPerTemplate()
     {
         $renderer = new PlatesRenderer(self::$assetDir, 'tpl');
-		
+        
         $name = 'Plates';
         $name2 = 'BitFrame';
         $renderer->addDefaultParam($renderer::TEMPLATE_ALL, ['name' => $name]);
         $renderer->addDefaultParam('plates2', ['name' => $name2]);
-		
+        
         $result = $renderer->render('plates');
         $content = file_get_contents(self::$assetDir . '/plates.tpl');
         $content = str_replace('<?= $name; ?>', $name, $content);
-		
+        
         $this->assertEquals($content, $result);
-		
+        
         $result = $renderer->render('plates2');
         $content = file_get_contents(self::$assetDir . '/plates2.tpl');
         $content = str_replace('<?= $name; ?>', $name2, $content);
-		
+        
         $this->assertEquals($content, $result);
     }
 
-	public function testOverwriteSharedParametersAtRender()
+    public function testOverwriteSharedParametersAtRender()
     {
         $renderer = new PlatesRenderer(self::$assetDir, 'tpl');
-		
+        
         $name = 'Plates';
         $name2 = 'BitFrame';
         $renderer->addDefaultParam($renderer::TEMPLATE_ALL, ['name' => $name]);
-		
+        
         $result = $renderer->render('plates', ['name' => $name2]);
         $content = file_get_contents(self::$assetDir . '/plates.tpl');
         $content = str_replace('<?= $name; ?>', $name2, $content);
-		
+        
         $this->assertEquals($content, $result);
     }
-	
-	
+    
+    
     public function assertTemplatePathString($path, TemplatePath $templatePath, $message = null)
     {
         $message = $message ?: sprintf('Failed to assert TemplatePath casts to string path %s', $path);
         $this->assertEquals($path, (string) $templatePath, $message);
     }
-	
+    
     public function assertTemplatePathNamespace($namespace, TemplatePath $templatePath, $message = null)
     {
         $message = $message ?: sprintf('Failed to assert TemplatePath namespace matched %s', var_export($namespace, 1));
         $this->assertEquals($namespace, $templatePath->getNamespace(), $message);
     }
-	
+    
     public function assertEmptyTemplatePathNamespace(TemplatePath $templatePath, $message = null)
     {
         $message = $message ?: 'Failed to assert TemplatePath namespace was empty';
         $this->assertEmpty($templatePath->getNamespace(), $message);
     }
-	
+    
     public function assertEqualTemplatePath(TemplatePath $expected, TemplatePath $received, $message = null)
     {
         $message = $message ?: 'Failed to assert TemplatePaths are equal';
@@ -304,23 +304,23 @@ class PlatesRendererTest extends TestCase
             $this->fail($message);
         }
     }
-	
-	/**
-	 * Call protected/private method of a class.
-	 *
-	 * @param object &$object Instantiated object that we will run method on.
-	 * @param string $methodName Method name to call
-	 * @param array $params Array of parameters to pass into method.
-	 *
-	 * @return mixed
-	 */
-	public function invokeMethod(&$object, $methodName, array $params = [])
-	{
-		$reflection = new \ReflectionClass(get_class($object));
-		
-		$method = $reflection->getMethod($methodName);
-		$method->setAccessible(true);
+    
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array $params Array of parameters to pass into method.
+     *
+     * @return mixed
+     */
+    public function invokeMethod(&$object, $methodName, array $params = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
 
-		return $method->invokeArgs($object, $params);
-	}
+        return $method->invokeArgs($object, $params);
+    }
 }
